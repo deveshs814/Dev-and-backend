981e064ec8c6062515dde268418629294c16834a
Devesh Shukla <shukladevesh9814@gmail.com>
2024-07-09 23:25:23 +0530

    Learned about middlewares and methods of handling api's errors.

--- src/app.js
+++ src/app.js
@@ -4,4 +4,15 @@ import cors from 'cors';
 
 const app = express();
 
+
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true
+}))
+
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended:true,limit:"15kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
 export {app};
